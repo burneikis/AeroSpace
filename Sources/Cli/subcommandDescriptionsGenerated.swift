bbf5d8864f7aa9bca6c70aab491eef060ce00d08
Alex Burneikis <alexburneikis@gmail.com>
2026-04-06 09:33:29 +1000

    command help docs

--- Sources/Cli/subcommandDescriptionsGenerated.swift
+++ Sources/Cli/subcommandDescriptionsGenerated.swift
@@ -19,6 +19,7 @@ let subcommandDescriptions = [
     ["  list-exec-env-vars", "List environment variables that exec-* commands and callbacks are run with"],
     ["  list-modes", "Print a list of modes currently specified in the configuration"],
     ["  list-monitors", "Print monitors that satisfy conditions"],
+    ["  list-tree", "Print the tree of windows and containers"],
     ["  list-windows", "Print windows that satisfy conditions"],
     ["  list-workspaces", "Print workspaces that satisfy conditions"],
     ["  macos-native-fullscreen", "Toggle macOS fullscreen for the focused window"],
