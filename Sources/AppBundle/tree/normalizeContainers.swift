92b0273711a9ab307265e3558a88498429785080
Alex Burneikis <alexburneikis@gmail.com>
2026-04-06 09:12:50 +1000

    we dont need this config override

--- Sources/AppBundle/tree/normalizeContainers.swift
+++ Sources/AppBundle/tree/normalizeContainers.swift
@@ -9,7 +9,7 @@ extension Workspace {
 
 extension TilingContainer {
     @MainActor fileprivate func unbindEmptyAndAutoFlatten() {
-        if let child = children.singleOrNil(), config.enableNormalizationFlattenContainers && !config.enableDwindleAutotiling && (child is TilingContainer || !isRootContainer) {
+        if let child = children.singleOrNil(), config.enableNormalizationFlattenContainers && (child is TilingContainer || !isRootContainer) {
             child.unbindFromParent()
             let mru = parent?.mostRecentChild
             let previousBinding = unbindFromParent()
