57b8159620f4e0b99179e1b4ae7f10ba2741339b
Alex Burneikis <alexburneikis@gmail.com>
2026-04-06 08:59:04 +1000

    simplify tree

--- Sources/Common/cmdArgs/impl/ListTreeCmdArgs.swift
+++ Sources/Common/cmdArgs/impl/ListTreeCmdArgs.swift
@@ -1,5 +1,6 @@
 public struct ListTreeCmdArgs: CmdArgs {
     /*conforms*/ public var commonState: CmdArgsCommonState
+    public var fullTree: Bool = false
     public init(rawArgs: StrArrSlice) { self.commonState = .init(rawArgs) }
     public static let parser: CmdParser<Self> = .init(
         kind: .listTree,
@@ -7,6 +8,7 @@ public struct ListTreeCmdArgs: CmdArgs {
         help: list_tree_help_generated,
         flags: [
             "--workspace": ArgParser(\.workspaceName, upcastArgParserFun(parseWorkspaceNameSubArg)),
+            "--full": trueBoolFlag(\.fullTree),
         ],
         posArgs: [],
     )
